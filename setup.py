368d70e3cc009ff960ef848c686d3c3a04a1bd89
Travis Dent <tcdent@gmail.com>
2013-10-29 15:14:19 -0700

    Fix setup.py version parsing to support multiple variables.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def parse_version(asignee):
     import os, re
     here = os.path.dirname(os.path.abspath(__file__))
     version_re = re.compile(
-        r'__version__ = (\(.*?\))')
+        r'%s = (\(.*?\))' % asignee)
     with open(os.path.join(here, 'django_assets', '__init__.py')) as fp:
         for line in fp:
             match = version_re.search(line)
